593a9768ea94b88f3fcd4e17805c064d6b90d6ed
RANZMENTRY <krismantarirani17@gmail.com>
2025-04-08 03:45:53 +0700

    Fix vite.config.js conflict

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/"
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 273d3c19eb348d75c8ea44034f8b14902a840364
