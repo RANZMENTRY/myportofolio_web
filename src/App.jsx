42e46e7cf90b8f78081ecf6310e7fa16361af602
RANZMENTRY <krismantarirani17@gmail.com>
2025-04-08 03:41:56 +0700

    Fix path error About.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Home";
-import About from "./components/About";
+import About from './components/About';
 import Memories from "./components/memories";
 import Skills from "./components/skill";
 import Sertif from "./components/sertif";
