669824929d49c01c81b66cbf7fc6c647d9386cbd
RANZMENTRY <152503463+RANZMENTRY@users.noreply.github.com>
2025-04-10 13:49:38 +0700

    Add files via upload

--- init-tailwind.js
+++ init-tailwind.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { exec } = require("child_process");
 
 exec("npx tailwindcss init -p", (error, stdout, stderr) => {
@@ -12,18 +11,3 @@ exec("npx tailwindcss init -p", (error, stdout, stderr) => {
   }
   console.log(`✅ Success:\n${stdout}`);
 });
-=======
-const { exec } = require("child_process");
-
-exec("npx tailwindcss init -p", (error, stdout, stderr) => {
-  if (error) {
-    console.error(`❌ Error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`⚠️ Stderr: ${stderr}`);
-    return;
-  }
-  console.log(`✅ Success:\n${stdout}`);
-});
->>>>>>> 273d3c19eb348d75c8ea44034f8b14902a840364
